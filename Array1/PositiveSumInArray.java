b219683eb6726844effce2bd53da9759a1da7676
Ayushi753 <pandeyayushi586@gmail.com>
2026-06-15 22:29:53 +0530

    Positive Sum In Array

---
+++ Array1/PositiveSumInArray.java
@@ -0,0 +1,30 @@
+package Array1;
+import java .util.*;
+public class PositiveSumInArray 
+{
+  public static void main(String args[])  
+  {
+    Scanner sc=new Scanner(System.in);
+    int a[]=new int [5];  //declare the size5
+     
+    //Read the data
+    System.out.println("Enter any 5 number");
+    for(int x=0; x<5; x++)
+    {
+        a[x]=sc.nextInt();
+    }
+    // Search the data
+    int sum=0;
+    for(int x=0; x<5;x++)
+    {
+        if (a[x]>0)
+        {
+           sum=sum+a[x] ;
+        }
+        
+    
+            System.out.println("sum ="+sum);
+        
+    }
+  }
+}
